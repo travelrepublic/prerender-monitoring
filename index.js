546602ccd59c0fe67fabec3c5af5b48e72fa45ea
mvisintin <mvisintin@travelrepublic.co.uk>
2015-04-27 16:38:56 +0100

    First Commit | In order to test the reverse proxy

---
+++ index.js
@@ -0,0 +1,20 @@
+var restify = require('restify');
+
+var PORT = 8080;
+
+var server = restify.createServer({
+	name: 'prerender-monitoring'
+});
+
+server
+  .use(restify.fullResponse())
+  .use(restify.bodyParser());
+
+server
+	.get('/', function translate (req, res, next) {
+		res.send(200, 'It works!');
+	});
+
+server.listen(PORT, function () {
+	console.log('sourcemaps-stacktrace-started on port %s', PORT);
+});
