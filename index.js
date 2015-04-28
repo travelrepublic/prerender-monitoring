fff7c845005d960d69b0ea4a892480fde77a4246
mvisintin <mvisintin@travelrepublic.co.uk>
2015-04-28 17:52:00 +0100

    Command | New test for script execution

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ server
 			fin = '';
 
 		conn.on('ready', function () {
-			conn.exec('bobo.sh', function (err, stream) {
+			conn.exec('bobo', function (err, stream) {
 				if (err) {
 					return res.send(500, err);
 				}
